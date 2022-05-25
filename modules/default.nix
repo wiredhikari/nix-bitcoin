3869f5802f4e38030f646f461f46cbc76d1c5ff5
Atharva <atharvaamritkar@protonmail.com>
2022-05-25 16:58:50 +0530

    Adding minimint in modules

--- modules/default.nix
+++ modules/default.nix
@@ -5,6 +5,7 @@
   default = ./default.nix;
   electrs = ./electrs.nix;
   liquid = ./liquid.nix;
+  minimint = ./minimint.nix;
   presets.secure-node = ./presets/secure-node.nix;
   rtl = ./rtl.nix;
   spark-wallet = ./spark-wallet.nix;
