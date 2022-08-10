d2a056fe7508d2fb212fa66755b094e804a33f36
Atharva <atharvaamritkar@protonmail.com>
2022-08-10 19:51:19 +0530

    testing new cfg dir

--- modules/minimint.nix
+++ modules/minimint.nix
@@ -74,6 +74,7 @@ in {
       requires = [ "bitcoind.service" ];
       after = [ "bitcoind.service" ];
       preStart = ''
+      mkdir -p ${cfg.dataDir} 
         ${config.nix-bitcoin.pkgs.minimint}/bin/configgen ${cfg.dataDir} 1 4000 5000 1 10 100 1000 10000 100000 1000000
 	sed -i -e "s/127.0.0.1:18443/${bitcoindRpcAddress}:${toString bitcoind.rpc.port}/g" ${cfg.dataDir}/server-0.json
 	sed -i -e 's/user": "bitcoin"/user": "${bitcoind.rpc.users.public.name}"/g' ${cfg.dataDir}/server-0.json
